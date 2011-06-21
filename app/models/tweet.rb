e4442e83c29e8ca99d3226a117757d3a738a7292
Jennifer Mace <englishmace@gmail.com>
2011-06-21 11:46:56 -0400

    Cleaned up Tweet model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,17 +1,8 @@
 class Tweet
 include MongoMapper::Document
-  key :data#, Hashie::Rash
+  key :data
   key :city_id, ObjectId
   key :user, ObjectId
   belongs_to :city
   has_one :tweeter
-  def wheres_wally
-    puts Twitter.user("wally").location
-  end
-  def wheres(username)
-    puts Twitter.user(username).location
-  end
-  def said(username)
-    Twitter.user_timeline(username).first
-  end
 end
