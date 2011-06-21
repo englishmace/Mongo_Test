9dac473464d0c3c92fbedb7217f5029425ddd22d
Jennifer Mace <englishmace@gmail.com>
2011-06-21 15:02:18 -0400

    init_tweets will now pull up to 3200 past tweets

--- app/models/city.rb
+++ app/models/city.rb
@@ -10,32 +10,41 @@ include MongoMapper::Document
     self.tweeters.each do |tweeter|
       new_tweets = Twitter.user_timeline(tweeter.user, :since_id => tweeter.latest_tweet, :trim_user => true)
 
-      if tweeter.latest_tweet != new_tweets.first.id
-        updated = true
-      tweeter.latest_tweet = new_tweets.first.id
-      tweeter.save
+      unless new_tweets.empty?
+        tweeter.latest_tweet = new_tweets.first.id
+        tweeter.save
 
-      for item in new_tweets do
-        Tweet.new(:data => item, :city_id => self.id, :user => tweeter.id).save
-      end
+        for item in new_tweets do
+          Tweet.new(:data => item, :city_id => self.id, :user => tweeter.id).save
+        end
+        updated = true
+      end 
     end
+
     return updated
   end
 
   # pulls all backdated tweets for a new tweeter; initializes their twit_id & latest
   def init_tweets(tweeter)
-    all_tweets = Twitter.user_timeline(tweeter.user, :trim_user => true, :count => 200)
+    all_tweets = Twitter.user_timeline(tweeter.user, :trim_user => true, :count => 200, :include_rts => true)
     tweeter.latest_tweet = all_tweets.first.id
     tweeter.twit_id = all_tweets.first.user.id
-    for item in all_tweets do
-      Tweet.new(:data => item, :city_id => self.id, :user => tweeter.id).save
+    page = 2
+
+    until all_tweets.empty?
+      for item in all_tweets do
+        Tweet.new(:data => item, :city_id => self.id, :user => tweeter.id, ).save
+      end
+      all_tweets = Twitter.user_timeline(tweeter.user, :trim_user => true, :count => 200, :include_rts => true, :page => page)
+      page += 1
     end
+
   end
 
   # initializes a new tweeter & calls init_tweets
   def add_tweeter(username)
     tweeter = Tweeter.new(:user => username, :city_id => self.id)
-    get_init_tweets(tweeter)
+    init_tweets(tweeter)
     tweeter.save
   end
 
