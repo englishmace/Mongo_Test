9e350dcfb5f821e4ca1eacf10354e7d177e2486f
Jennifer Mace <englishmace@gmail.com>
2011-06-20 21:03:08 -0400

    get_tweets now working on a per-tweeter basis

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,13 +2,15 @@ class City
 include MongoMapper::Document
   has_many :tweets
   has_many :tweeters
+
   def get_tweets(tweeter)
-    tweets = Twitter.user_timeline(tweeter.user, :since_id => tweeter.latest_tweet)
-    tweeter.lastest_tweet = tweets.first.id
-    for item in tweets do
+    new_tweets = Twitter.user_timeline(tweeter.user, :since_id => tweeter.latest_tweet, :trim_user => true)
+    tweeter.latest_tweet = new_tweets.first.id
+    for item in new_tweets do
       Tweet.new(:data => item, :city_id => self.id, :user => tweeter.id).save
     end
   end
+
   def get_first_tweet(tweeter)
     temp = Twitter.user_timeline(tweeter.user).first
     Tweet.new(:city_id => self.id, :user => tweeter.id, :data => temp).save
